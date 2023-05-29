70ad9cdb639a02e7a7f21c4e14b210add590829e
Zhivar <zhivarsourati@gmail.com>
2023-05-29 18:16:09 -0400

    Add the new datasets with 200 successful attacks

--- normal_models/train_vanilla_model.sh
+++ normal_models/train_vanilla_model.sh
@@ -1,28 +1,32 @@
 ################################ Training ################################
 
+dataset="ag_news"
+num_labels=4
+
 echo "Mode" $1
 
 if [ "$1" = "train" ]; then
 
-    TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=2,3 python train_vanilla_model.py \
+    TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 python vanilla_model.py \
         --mode train \
-        --batch_size 512 \
+        --batch_size 256 \
+        --max_length 64 \
         --logging_steps 50 \
-        --data_dir "data/glue_data/sst2/"
+        --data_dir "../datasets/${dataset}_dataset" \
+        --num_labels ${num_labels} \
+        --model_dir "models/${dataset}"
 
 ################################ Testing ################################
 
 else
 
-    TOKENIZERS_PARALLELISM=false WANDB_MODE="offline" CUDA_VISIBLE_DEVICES=2 python train_vanilla_model.py \
-        --mode eval \
-        --batch_size 512 \
+    TOKENIZERS_PARALLELISM=false WANDB_MODE="offline" CUDA_VISIBLE_DEVICES=2 python vanilla_model.py \
+        --mode test \
+        --batch_size 256 \
+        --max_length 64 \
         --logging_steps 50 \
-        --data_dir "data/glue_data/sst2/"
+        --data_dir "../datasets/${dataset}_dataset" \
+        --num_labels ${num_labels} \
+        --model_dir "models/${dataset}"
 
-    TOKENIZERS_PARALLELISM=false WANDB_MODE="offline" CUDA_VISIBLE_DEVICES=2 python train_vanilla_model.py \
-        --mode eval_adv \
-        --batch_size 512 \
-        --logging_steps 50 \
-        --data_dir "data/glue_data/sst2/"
 fi
