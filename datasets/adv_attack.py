4d7fd5cd69366fe343f68e3016a2909d16f6a81e
Zhivar <zhivarsourati@gmail.com>
2023-05-29 01:10:18 -0400

    Add the dataset with their adversarial counterparts as a separate folder

--- datasets/adv_attack.py
+++ datasets/adv_attack.py
@@ -1,6 +1,7 @@
 import argparse
+import textattack
 from textattack.transformations import WordSwapRandomCharacterDeletion, BackTranslation
-
+import transformers
 from textattack.transformations import CompositeTransformation
 from textattack.constraints.pre_transformation import (
     RepeatModification,
@@ -25,6 +26,7 @@ def main():
     parser.add_argument(
         "--attack_type", type=str, default="textfooler", help="attack type"
     )
+    parser.add_argument("--dataset", type=str, default="imdb", help="dataset to use")
 
     args = parser.parse_args()
 
@@ -39,26 +41,41 @@ def main():
     # dataset = pd.read_csv("test.csv")
     # sentences = dataset["sentence"].tolist()
 
-    import transformers
-
-    model = transformers.AutoModelForSequenceClassification.from_pretrained(
-        "textattack/bert-base-uncased-imdb"
-    )
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-        "textattack/bert-base-uncased-imdb"
-    )
-    import textattack
+    if args.dataset == "agnews":
+        model = transformers.AutoModelForSequenceClassification.from_pretrained(
+            "textattack/bert-base-uncased-ag-news"
+        )
+        tokenizer = transformers.AutoTokenizer.from_pretrained(
+            "textattack/bert-base-uncased-ag-news"
+        )
+    elif args.dataset == "imdb":
+        model = transformers.AutoModelForSequenceClassification.from_pretrained(
+            "textattack/bert-base-uncased-imdb"
+        )
+        tokenizer = transformers.AutoTokenizer.from_pretrained(
+            "textattack/bert-base-uncased-imdb"
+        )
 
     model_wrapper = textattack.models.wrappers.HuggingFaceModelWrapper(model, tokenizer)
-    dataset = textattack.datasets.HuggingFaceDataset("imdb", split="test")
-    attack = textattack.attack_recipes.TextFoolerJin2019.build(model_wrapper)
+
+    if args.dataset == "agnews":
+        dataset = textattack.datasets.HuggingFaceDataset("ag_news", split="test")
+    elif args.dataset == "imdb":
+        dataset = textattack.datasets.HuggingFaceDataset("imdb", split="test")
+
+    if args.attack_type == "textfooler":
+        attack = textattack.attack_recipes.TextFoolerJin2019.build(model_wrapper)
+    elif args.attack_type == "textbugger":
+        attack = textattack.attack_recipes.TextBuggerLi2018.build(model_wrapper)
+
     # Attack 20 samples with CSV logging and checkpoint saved every 5 interval
     attack_args = textattack.AttackArgs(
+        random_seed=1234,
         num_examples=200,
         shuffle=True,
-        log_to_csv="log_textfooler.csv",
-        log_summary_to_json="summary_textfooler.json",
-        checkpoint_interval=5,
+        log_to_csv=f"log_{args.dataset}_{args.attack_type}.csv",
+        log_summary_to_json=f"summary_{args.dataset}_{args.attack_type}.json",
+        checkpoint_interval=None,
         checkpoint_dir="checkpoints",
         disable_stdout=True,
         parallel=True,
