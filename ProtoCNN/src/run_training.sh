ad048f0739f3f3d0f73498633e9c409f011f1f2f
Darshan Deshpande <darshang@ckg10.isi.edu>
2023-06-09 15:31:05 -0400

    Adopted softmax for training

--- ProtoCNN/src/run_training.sh
+++ ProtoCNN/src/run_training.sh
@@ -4,12 +4,12 @@ eval "$(conda shell.bash hook)"
 conda activate prototype_learning
 # nvidia-smi
 
-for type in "without_clustering_loss" "with_linear" "without_dynamic"; do
     echo "----------- $type -----------"
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 python3 train_custom.py \
+for type in "with_linear" "without_clustering_loss" "without_separation_loss" "without_dynamic"; do
+    CUDA_VISIBLE_DEVICES=1,3,6,7 python3 train_custom.py \
         --type $type \
-        --dataset_name="dbpedia_dataset" \
-        --num_labels=9
+        --dataset_name="imdb_dataset" \
+        --num_labels=2
 done
 
 conda deactivate
