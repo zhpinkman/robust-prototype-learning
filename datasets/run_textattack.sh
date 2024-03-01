e45a89fa11531adf86515feef1c5b26f9a604b29
Zhivar <zhivarsourati@gmail.com>
2024-03-01 14:58:08 -0800

    change the order of attacks

--- datasets/run_textattack.sh
+++ datasets/run_textattack.sh
@@ -1,6 +1,6 @@
 for dataset in "imdb" "ag_news" "dbpedia"; do
     # dataset="olid"
-    for attack_type in "deepwordbug" "a2t" "checklist" "hotflip" "iga" "bae" "input_reduction" "kuleshov" "swarm" "pwws" "clare" "pruthi"; do
+    for attack_type in "hotflip" "iga" "bae" "input_reduction" "kuleshov" "swarm" "pwws" "clare" "pruthi" "deepwordbug" "a2t"; do # "checklist"
         if [ "$dataset" = "ag_news" ]; then
             for model_checkpoint in "../normal_models/models/ag_news_prajjwal1/bert-medium" "textattack/roberta-base-ag-news" "textattack/bert-base-uncased-ag-news" "andi611/distilbert-base-uncased-ner-agnews" "../normal_models/models/ag_news_ModelTC/bart-base-mnli" "../normal_models/models/ag_news_google/electra-base-discriminator"; do
                 echo " Attack type: " $attack_type
